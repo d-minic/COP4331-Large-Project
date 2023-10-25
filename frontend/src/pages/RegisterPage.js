2ff114f4cd921c6498d7577382b4351ceff28251
exclusive321 <103155528+exclusive321@users.noreply.github.com>
2023-10-25 18:24:24 -0400

    Add files via upload

---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import Register from '../components/RegisterPage';
+const LoginPage = () =>
+{
+    return(
+        <div>
+        <PageTitle />
+        <Register/>
+        </div>
+    );
+};
+export default RegisterPage;
