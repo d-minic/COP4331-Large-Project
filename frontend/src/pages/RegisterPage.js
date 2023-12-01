706eae920b80033a125d83f1c1c50682fc75f5aa
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-01 00:24:23 -0500

    Update RegisterPage.js

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,52 +1,21 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Button } from 'react-native';
 import PageTitle from '../components/PageTitle';
-import Register from '../components/Register'; // Assuming this is the correct path
+import Register from '../components/Register';
+import { useNavigation } from '@react-navigation/native';
 
 const RegisterPage = () => {
-  const navigation = useNavigation();
 
-  const handleRegistration = () => {
-    // Handle registration logic if needed
-    console.log('Registration button pressed');
-  };
+    const navigation = useNavigation();
 
-  return (
-    <View style={styles.container}>
-      <PageTitle title="Register" />
-      <Register />
-      <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="First Name" />
-        <TextInput style={styles.input} placeholder="Last Name" />
-        <TextInput style={styles.input} placeholder="Email" />
-        <TextInput style={styles.input} placeholder="Username" />
-        <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} />
-        <Button title="Register" onPress={handleRegistration} />
-      </View>
-      <Text>Already have an account? <Text style={styles.link} onPress={() => navigation.navigate('LoginPage')}>Log in here</Text></Text>
-    </View>
-  );
+    return (
+        <View>
+            <Button
+            title = "Go to Register Page"
+            onPress={()=> navigation.navigate('RegisterPage')}
+            />
+        </View>
+    );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    padding: 10,
-  },
-  link: {
-    color: 'blue',
-    textDecorationLine: 'underline',
-  },
-});
 
 export default RegisterPage;
