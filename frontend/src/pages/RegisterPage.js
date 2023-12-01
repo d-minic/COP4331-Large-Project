3ede128f204ec16181aed38f474b538fc630bfd4
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-01 02:18:58 -0500

    Update RegisterPage.js

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
 import Register from '../components/Register';
-import './RegisterPage.css'; // Import the CSS file
+
 
 const RegisterPage = () => {
     return (
-      <div className="container">
-      <h1>Register</h1>
-        <button type="submit">Register</button>
+      <div>
+        <PageTitle title ="Register"/>
     <Register/>
     </div>
     );
