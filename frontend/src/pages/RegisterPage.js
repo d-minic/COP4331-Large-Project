e700bcb87fa744d02c94bcb4ada0b931446746e1
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-01 01:37:26 -0500

    Update RegisterPage.js

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -5,12 +5,28 @@ import './RegisterPage.css';
 
 const RegisterPage = () => {
     return (
-       <div className="container">
-       <h1>Register</h1>
+      <div className="container">
+      <h1>Register</h1>
+      <form id="registerForm">
+        <label htmlFor="firstName">First Name:</label>
+        <input type="text" id="firstName" name="firstName" required />
+        <label htmlFor="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="lastName" required />
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" name="email" required />
+        <label htmlFor="userName">UserName:</label>
+        <input type="text" id="userName" name="userName" required />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+        <button type="submit">Register</button>
     <Register/>
-     
+      </form>
+      <p id="registerMessage"></p>
+      <p>Already have an account? <a href="login.html">Log in here</a></p>
+    
     </div>
   );
 };
 
 export default RegisterPage;
+
