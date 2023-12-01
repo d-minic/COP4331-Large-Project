af0c2f8da5fec6728447026249596332e56ce91a
Dani <daniella.figueroa10@gmail.com>
2023-12-01 13:13:42 -0500

    font change

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -7,7 +7,7 @@ function LandingPage() {
   return (
     <div className="landing-container">
       <img src={logo} alt="SmartTooth Logo" className="logo" />
-      <p>The free and shark learning tool for students K-12</p>
+      <p className="p-description">The free and shark learning tool for students K-12</p>
       <div className="button-container">
         <Link to="/login">
           <button className="btn login-btn">Login</button>
