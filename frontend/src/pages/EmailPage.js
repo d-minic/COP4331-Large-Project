4e75c9ab51feed89e4f81b20a3050d39a97c9110
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-01 10:14:34 -0500

    Update EmailPage.js

--- frontend/src/pages/EmailPage.js
+++ frontend/src/pages/EmailPage.js
@@ -1,1 +1,13 @@
 // email page needs to go here
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import Login from '../components/EmailVerification';
+const EmailPage = () =>
+{
+    return(
+        <div>
+        <EmailVerification />
+        </div>
+    );
+};
+export default EmailPage;
