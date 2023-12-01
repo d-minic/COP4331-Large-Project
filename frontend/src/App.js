bab2c266e7de2342cd6eccd404d3781bf187e986
exclusive321 <103155528+exclusive321@users.noreply.github.com>
2023-12-01 11:30:37 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 import RegisterPage from './pages/RegisterPage';
@@ -8,7 +9,8 @@ function App() {
 return (
 <BrowserRouter>
 <Routes>
-<Route path="/" element={<LoginPage />} />
+<Route path="/" element={<LandingPage />} />
+<Route path="/login" element={<LoginPage />} />
 <Route path="/register" element={<RegisterPage />} />
 <Route path="/cards" element={<CardPage />} />
 </Routes>
