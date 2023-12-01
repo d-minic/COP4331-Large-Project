3a3b0c235214a452ff04566eecd6ab4ef34e1fc4
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-01 09:20:10 -0500

    Update LoggedInName.js

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -20,4 +20,30 @@ onClick={doLogout}> Log Out </button>
 </div>
 );
 };
+const styles = {
+    container: {
+        textAlign: 'center',
+        padding: '20px',
+        backgroundColor: '#f0f0f0',
+        borderRadius: '8px',
+        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+        margin: '20px',
+    },
+    userName: {
+        fontSize: '18px',
+        fontWeight: 'bold',
+        color: '#333',
+        marginBottom: '10px',
+    },
+    logoutButton: {
+        backgroundColor: '#c3e6f2',
+        color: '#000000',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        transition: 'background-color 0.3s, color 0.3s',
+    },
+};
+
 export default LoggedInName;
