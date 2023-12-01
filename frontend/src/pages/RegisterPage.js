50b78bfdcbdacd2af0573785aaed31e0c4570ccf
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-01 01:54:25 -0500

    Update RegisterPage.js

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
 import Register from '../components/Register';
-
+import './RegisterPage.css'; // Import the CSS file
 
 const RegisterPage = () => {
     return (
