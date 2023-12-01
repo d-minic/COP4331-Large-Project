ed7edf9166f2f58839b0232930fae590e9ef199b
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-01 01:54:13 -0500

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './RegisterPage.css'; // Import the CSS file
+
 
 function Register() {
     var registerName;
