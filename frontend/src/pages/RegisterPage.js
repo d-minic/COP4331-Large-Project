ec65de50b465bdbf2530c94e911336843da25928
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-01 00:32:03 -0500

    Update RegisterPage.js

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button } from 'react-native'; // Remove this line
+
 import PageTitle from '../components/PageTitle';
 import Register from '../components/Register';
 import { useNavigation } from '@react-navigation/native';
