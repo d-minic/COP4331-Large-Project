72da3d1bb7548a21de1f7259134bca51ac3ac105
Alex <acharkiewicz27@gmail.com>
2023-12-03 18:02:50 -0500

    Add files via upload

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import Register from '../components/Home';
+
+
+const HomePage = () => {
+    return (
+      <div>
+    <Home/>
+    </div>
+    );
+};
+
+export default HomePage;
