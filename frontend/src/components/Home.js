bac220c7c52307006267556fe0147fb1d0ae1620
Alex <acharkiewicz27@gmail.com>
2023-12-03 18:56:02 -0500

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,24 +5,24 @@ import './HomePage.css';
 function Home() {
     return (
     <div id="homeDiv">
-            <h1 style="margin-top: 100px; margin-left: 20px">Recent:</h1>
-    <div class="wrapper" style="margin-top: 20px;">
-        <div class="item">Biology 1</div>
-        <div class="item">Biology 2</div>
-        <div class="item">Chemistry 1</div>
-        <div class="item">Chemistry 2</div>
-        <div class="item">Physics 1</div>
-        <div class="item">Physics 2</div>
-    </div>
-    <h1 style="margin-top: 100px; margin-left: 20px">Popular:</h1>
-    <div class="wrapper" style="margin-top: 20px;">
-        <div class="item">Biology 1</div>
-        <div class="item">Biology 2</div>
-        <div class="item">Chemistry 1</div>
-        <div class="item">Chemistry 2</div>
-        <div class="item">Physics 1</div>
-        <div class="item">Physics 2</div>
-    </div>
+        <h1>Recent:</h1>
+        <div class="wrapper">
+            <div class="item">Biology 1</div>
+            <div class="item">Biology 2</div>
+            <div class="item">Chemistry 1</div>
+            <div class="item">Chemistry 2</div>
+            <div class="item">Physics 1</div>
+            <div class="item">Physics 2</div>
+        </div>
+        <h1>Popular:</h1>
+        <div class="wrapper">
+            <div class="item">Biology 1</div>
+            <div class="item">Biology 2</div>
+            <div class="item">Chemistry 1</div>
+            <div class="item">Chemistry 2</div>
+            <div class="item">Physics 1</div>
+            <div class="item">Physics 2</div>
+        </div>
     </div>
     );
 }
