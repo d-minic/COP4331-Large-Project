0f687165d6d2b35cdc31ff8be6bfb9c0c7c5ea14
Alex <acharkiewicz27@gmail.com>
2023-12-03 19:07:48 -0500

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,17 +5,7 @@ import './HomePage.css';
 function Home() {
     return (
     <div id="homeDiv">
-        <!doctype html>
-        <html lang="en">
-          <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Home</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-            <link rel="stylesheet" href="HomePage.css">
-          </head>
-          <body>
-            
+        
             <nav class="navbar fixed-top navbar-expand-lg navbar-light fs-5">
                 <a href="#" class="navbar-brand mb-0 h1">
                     <img class="d-inline-block align-top" src="smarttoothlogo.PNG" width="200" >
@@ -91,9 +81,6 @@ function Home() {
                 <div class="item">Physics 1</div>
                 <div class="item">Physics 2</div>
             </div>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-          </body>
-        </html>
     </div>
     );
 }
