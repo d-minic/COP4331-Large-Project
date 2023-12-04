ea91e741b8c3edf7440bc9b38152bbff160f2f63
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-03 20:45:08 -0500

    Update Exam.js

--- frontend/src/components/Exam.js
+++ frontend/src/components/Exam.js
@@ -5,6 +5,18 @@ const Exam = () => {
   const [questions, setQuestions] = useState([]);
   const [userAnswers, setUserAnswers] = useState({});
   const [score, setScore] = useState(0);
+   const app_name = 'smart-tooth-577ede9ea626'
+    function buildPath(route)
+    {
+        if (process.env.NODE_ENV === 'production')
+        {
+            return 'https://' + app_name + '.herokuapp.com/' + route;
+        }
+        else
+        {
+         return 'http://localhost:5000/' + route;
+        }
+    }
 
   useEffect(() => {
     // Fetch questions from the 'getquestions' endpoint when the component mounts
@@ -21,18 +33,7 @@ const Exam = () => {
     fetchQuestions();
   }, []);
 
-   const app_name = 'smart-tooth-577ede9ea626'
-    function buildPath(route)
-    {
-        if (process.env.NODE_ENV === 'production')
-        {
-            return 'https://' + app_name + '.herokuapp.com/' + route;
-        }
-        else
-        {
-         return 'http://localhost:5000/' + route;
-        }
-    }
+   
   const handleAnswerClick = (questionId, answerId) => {
     setUserAnswers((prevAnswers) => ({
       ...prevAnswers,
