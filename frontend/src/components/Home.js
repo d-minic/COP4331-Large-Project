73b83b380a7ca8de8c999139feaabfa0ec595bf1
Alex <acharkiewicz27@gmail.com>
2023-12-04 15:56:22 -0500

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,66 +6,24 @@ import './HomePage.css';
 function Home() {
     return (
     <div id="homeDiv">
-        
-            <nav class="navbar fixed-top navbar-expand-lg navbar-light fs-5">
-                <a href="#" class="navbar-brand mb-0 h1">
-                    <img class="d-inline-block align-top" src="smarttoothlogo.PNG" width="200" ></img>
-                    
-                </a>
-                <button
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNav"
-                class="navbar-toggler"
-                aria-controls="navbar"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="navbar-item-active">
-                            <a href="#" class="nav-link active">
-                                <h4>Home</h4>
-                            </a>
-                        </li>
-                        <li class="navbar-item-active">
-                            <a href="#" class="nav-link">
-                                <h4>Create</h4>
-                            </a>
-                        </li>
-                        <li class="navbar-item-active">
-                            <a href="#" class="nav-link">
-                                <h4>Browse</h4>
-                            </a>
-                        </li>
-                        <li class="navbar-item-active">
-                            <a href="#" class="nav-link">
-                                <h4>Leaderboard</h4>
-                            </a>
-                        </li>
-                        <li class="navbar-item-active">
-                            <a href="#" class="nav-link">
-                                <h4>Friends</h4>
-                            </a>
-                        </li>
-                        <li class="navbar-item-active">
-                            <a href="#" class="nav-link">
-                                <h4>Profile</h4>
-                            </a>
-                        </li>
-                        <li class="navbar-item-active">
-                            <a href="#" class="nav-link">
-                                <h4>Logout</h4>
-                            </a>
-                        </li>
-                        
-                    </ul>
-                </div>
-            </nav>
-        
-            <h1 >Recent:</h1>
-            <div class="wrapper">
+        <nav class="navbar">
+            <ul>
+                <img src="smarttoothlesspixel.PNG" height="80">
+                
+                <li><a href="">Logout</a></li>
+                <li><a href="">Profile</a></li>
+                <li><a href="">Friends</a></li>
+                <li><a href="">Leaderboard</a></li>
+                <li><a href="">Browse</a></li>
+                <li><a href="">Create</a></li>
+                <li><a href="">Home</a></li>
+                
+            </ul>
+        </nav>
+    
+        <main>
+            <h1 style="margin-top: 100px; margin-left: 20px">Recent:</h1>
+            <div class="wrapper" style="margin-top: 20px;">
                 <div class="item">Biology 1</div>
                 <div class="item">Biology 2</div>
                 <div class="item">Chemistry 1</div>
@@ -73,8 +31,8 @@ function Home() {
                 <div class="item">Physics 1</div>
                 <div class="item">Physics 2</div>
             </div>
-            <h1 >Popular:</h1>
-            <div class="wrapper" >
+            <h1 style="margin-top: 100px; margin-left: 20px">Popular:</h1>
+            <div class="wrapper" style="margin-top: 20px;">
                 <div class="item">Biology 1</div>
                 <div class="item">Biology 2</div>
                 <div class="item">Chemistry 1</div>
@@ -82,6 +40,7 @@ function Home() {
                 <div class="item">Physics 1</div>
                 <div class="item">Physics 2</div>
             </div>
+        </main>
     </div>
     );
 }
