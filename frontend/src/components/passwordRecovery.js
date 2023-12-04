063994617068266098e4a28796abfb5499637802
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-03 19:18:15 -0500

    Create passwordRecovery.js

---
+++ frontend/src/components/passwordRecovery.js
@@ -0,0 +1,73 @@
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import './passwordRecovery.css'; // Import the CSS file for Forgot Password
+
+function ForgotPassword() {
+  const loginRef = useRef(null);
+
+
+  const [message, setMessage] = useState('');
+  const app_name = 'smart-tooth-577ede9ea626';
+
+  function buildPath(route) {
+    if (process.env.NODE_ENV === 'production') {
+      return 'https://' + app_name + '.herokuapp.com/' + route;
+    } else {
+      return 'http://localhost:5000/' + route;
+    }
+  }
+
+  const doForgotPassword = async (event) => {
+    event.preventDefault();
+    const login = loginRef.current.value;
+    const newPassword= passwordRef.current.value;
+    const verificationCode=codeRef.current.value;
+  
+
+    const obj = { login, newPassword, verificationCode};
+    const js = JSON.stringify(obj);
+
+    try {
+      const response = await fetch(buildPath('api/resetpassword'), {
+        method: 'POST',
+        body: js,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const res = await response.json();
+
+      if (res.error) {
+        setMessage(res.error);
+      } else {
+        setMessage('Password reset successful. Redirecting to login...');
+        setTimeout(() => {
+          window.location.href = '/login';
+        }, 2000);
+      }
+    } catch (e) {
+      alert(e.toString());
+    }
+  };
+
+  return (
+    <div id="forgotPasswordDiv">
+      <form onSubmit={doForgotPassword}>
+        <span id="inner-title">FORGOT PASSWORD</span>
+        <br />
+        <input type="text" id="login" placeholder="Username" ref={loginRef} />
+        <input
+          type="submit"
+          id="resetPasswordButton"
+          className="buttons"
+          value="Reset Password"
+          onClick={doForgotPassword}
+        />
+      </form>
+      <span id="resetPasswordMessage">{message}</span>
+    </div>
+  );
+}
+
+export default passwordRecovery;
