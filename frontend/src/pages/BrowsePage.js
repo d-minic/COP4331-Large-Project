f9e8cd4905a05df864aea6cd5ef475d1d9e17127
Alex <acharkiewicz27@gmail.com>
2023-12-04 19:12:09 -0500

    Create BrowsePage.js

---
+++ frontend/src/pages/BrowsePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import Browse from '../components/Browse';
+const BrowsePage = () =>
+{
+    return(
+        <div>
+        <Browse />
+        </div>
+    );
+};
+export default BrowsePage;
