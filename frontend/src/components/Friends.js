6abe3b3e9c1261287b7e9c1cece28f8de0ded4b0
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-04 23:28:34 -0500

    Update Friends.js

--- frontend/src/components/Friends.js
+++ frontend/src/components/Friends.js
@@ -1,10 +1,13 @@
+/// friends.js original working code
+
+
+
 import React, { useState, useEffect } from 'react';
 import './Friends.css'; // Import the CSS file
 
 const Friends = () => {
   const [friendsData, setFriendsData] = useState([]);
-  const [userId, setUserId] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
+  const [userId, setUserId] = useState(''); 
 
   const app_name = 'smart-tooth-577ede9ea626';
 
@@ -16,75 +19,33 @@ const Friends = () => {
     }
   }
 
-  const fetchFriends = async () => {
-    try {
-      const response = await fetch(buildPath('/api/getfriends'), {
-        method: 'POST',
-        body: JSON.stringify({ id: userId, search: searchQuery }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-
-      const data = await response.json();
-      setFriendsData(data.results);
-    } catch (error) {
-      console.error('Error fetching getfriends:', error);
-    }
-  };
-
-  const addFriend = async (friendId) => {
-    try {
-      const response = await fetch(buildPath('/api/addfriend'), {
-        method: 'POST',
-        body: JSON.stringify({ id1: userId, id2: friendId }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-
-      const result = await response.json();
-
-      if (result.error) {
-        console.error('Error adding friend:', result.error);
-      } else {
-        console.log('Friend added successfully!');
-        // You may want to fetch friends again to update the list after adding a friend
-        fetchFriends();
-      }
-    } catch (error) {
-      console.error('Error adding friend:', error);
-    }
-  };
-
   useEffect(() => {
-    // Assuming you have a way to get the user ID
-    const loggedInUserId = ''; // Replace this with your logic to get the logged-in user ID
-    setUserId(loggedInUserId);
+    const fetchFriends = async () => {
+      try {
+        const response = await fetch(buildPath('/api/getfriends'), {
+          method: 'POST',
+          body: JSON.stringify({ id: "652766a0b68e0abb7e3a2851" }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        const data = await response.json();
+        setFriendsData(data.results);
+      } catch (error) {
+        console.error('Error fetching getfriends:', error);
+      }
+    };
 
     fetchFriends();
-  }, [searchQuery]);
-
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
+  }, [userId]);
 
   return (
     <div id="friends">
       <h1>Your Friends</h1>
-      <div>
-        <label htmlFor="search">Search Friends:</label>
-        <input
-          type="text"
-          id="search"
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
-      </div>
       <ul>
         {friendsData.map((friend) => (
           <li key={friend._id}>
             <span className="friendName">{friend.Login}</span>
             <span className="friendScore">{friend.Points}</span>
-            <button onClick={() => addFriend(friend._id)}>
-              Add Friend
-            </button>
           </li>
         ))}
       </ul>
