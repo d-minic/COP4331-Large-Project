d48dfc3d72079b123ed07ccd0ca92c6bc1966aad
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-04 22:10:19 -0500

    Create Friends.js

---
+++ frontend/src/components/Friends.js
@@ -0,0 +1,54 @@
+// Friends.js
+
+import React, { useState, useEffect } from 'react';
+import './Friends.css'; // Import the CSS file
+
+const Friends = () => {
+  const [friendsData, setFriendsData] = useState([]);
+  const [userId, setUserId] = useState(''); // Assuming you have a way to get the user ID
+
+  const app_name = 'smart-tooth-577ede9ea626';
+
+  function buildPath(route) {
+    if (process.env.NODE_ENV === 'production') {
+      return `https://${app_name}.herokuapp.com${route}`;
+    } else {
+      return `http://localhost:5000${route}`;
+    }
+  }
+
+  useEffect(() => {
+    const fetchFriends = async () => {
+      try {
+        const response = await fetch(buildPath('/api/getfriends'), {
+          method: 'POST',
+          body: JSON.stringify({ id: userId }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        const data = await response.json();
+        setFriendsData(data.results);
+      } catch (error) {
+        console.error('Error fetching getfriends:', error);
+      }
+    };
+
+    fetchFriends();
+  }, [userId]);
+
+  return (
+    <div id="friends">
+      <h1>Your Friends</h1>
+      <ul>
+        {friendsData.map((friend) => (
+          <li key={friend._id}>
+            <span className="friendName">{friend.Login}</span>
+            <span className="friendScore">{friend.Points}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Friends;
