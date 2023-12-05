21342262eee7b615658a0bed9ee6f1e9b6163a8b
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-04 22:11:45 -0500

    Create FriendsPage.js

---
+++ frontend/src/pages/FriendsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import Friends from '../components/Friends';
+const FriendsPage = () =>
+{
+    return(
+        <div>
+        <Friends/>
+        </div>
+    );
+};
+export default FriendsPage;
