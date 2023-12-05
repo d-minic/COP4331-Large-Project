9bc14ba6896e2e36604afcf926da5b1b24b34591
Alex <acharkiewicz27@gmail.com>
2023-12-04 19:17:12 -0500

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -13,7 +13,7 @@ function Home() {
                 <li><a href="">Profile</a></li>
                 <li><a href="">Friends</a></li>
                 <li><a href="Leaderboard">Leaderboard</a></li>
-                <li><a href="">Browse</a></li>
+                <li><a href="Browse">Browse</a></li>
                 <li><a href="">Create</a></li>
                 <li><a href="home">Home</a></li>
                 
