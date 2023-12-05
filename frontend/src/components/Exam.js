4cfe76020142d83ae4641c13206fe88392f8bd52
Sarah <sarah.wheatfield@gmail.com>
2023-12-04 19:02:55 -0500

    Update to exam page

--- frontend/src/components/Exam.js
+++ frontend/src/components/Exam.js
@@ -3,28 +3,100 @@ import './examPage.css'; // Import the CSS file
 
 const Exam = () => {
   const [questions, setQuestions] = useState([]);
-  const [userAnswers, setUserAnswers] = useState({});
-  const [score, setScore] = useState(0);
-   const app_name = 'smart-tooth-577ede9ea626'
-    function buildPath(route)
+  const [questionInfo, setQuestionInfo] = useState({});
+  const [isTestCompleted, setIsTestCompleted] = useState(false);
+  const app_name = 'smart-tooth-577ede9ea626'
+
+  function buildPath(route)
+  {
+      if (process.env.NODE_ENV === 'production')
+      {
+          return 'https://' + app_name + '.herokuapp.com/' + route;
+      }
+      else
+      {
+        return 'http://localhost:5000/' + route;
+      }
+  }
+
+  const handleAnswerClick = async (questionId, selectedAnswer) => {
+  const correctAnswer = questionInfo[questionId]?.correctAnswer;
+
+  try
+  {
+    const obj = 
     {
-        if (process.env.NODE_ENV === 'production')
-        {
-            return 'https://' + app_name + '.herokuapp.com/' + route;
-        }
-        else
-        {
-         return 'http://localhost:5000/' + route;
-        }
+      id: "65623cb210dcacc0c1486814",
+      testId: "6564f0a624cfd1bdc400901e",
+      questionId: questionId,
+      correct: selectedAnswer === correctAnswer,
+    };
+    var js = JSON.stringify(obj);
+
+    const response = await fetch(buildPath('api/answerquestion'), {
+      method: 'POST',
+      body: js,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const result = await response.json();
+    console.log('Answer submitted:', result);
+
+    } catch (error) {
+      console.error('Error answering question:', error);
     }
+  };
+
+  const handleCompleteTest = async () => {
+    // Call the endpoint to complete the test
+    try {
+
+      const obj = 
+      {
+        id: "65623cb210dcacc0c1486814",
+        testId: "6564f0a624cfd1bdc400901e"
+      };
+      var js = JSON.stringify(obj);
+  
+      const response = await fetch(buildPath('api/answerquestion'), {
+        method: 'POST',
+        body: js,
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      const result = await response.json();
+      console.log('Answer submitted:', result);     
+
+
+     
+      setIsTestCompleted(true);
+    } catch (error) {
+      console.error('Error completing test:', error);
+    }
+  };
 
   useEffect(() => {
     // Fetch questions from the 'getquestions' endpoint when the component mounts
     const fetchQuestions = async () => {
-      try {
-        const response = await fetch(buildPath('/api/getquestions'));
+    var obj = {id:"6564f0a624cfd1bdc400901e"};
+    var js = JSON.stringify(obj);
+      try 
+      {
+        const response = await fetch(buildPath('api/getquestions'),
+        {method:'POST',body:js,headers:{'Content-Type':
+        'application/json'}});
         const data = await response.json();
-        setQuestions(data.questions);
+        setQuestions(data.results);
+        const info = {};
+        data.results.forEach((question) => {
+          info[question._id] = {
+            numberAnswers: question.NumberAnswers,
+            correctAnswer: question.CorrectAnswer,
+            subject: question.Subject,
+          };
+
+        setQuestionInfo(info);
+        });
       } catch (error) {
         console.error('Error fetching questions:', error);
       }
@@ -33,79 +105,29 @@ const Exam = () => {
     fetchQuestions();
   }, []);
 
-   
-  const handleAnswerClick = (questionId, answerId) => {
-    setUserAnswers((prevAnswers) => ({
-      ...prevAnswers,
-      [questionId]: answerId,
-    }));
-  };
-
-  const handleSubmitTest = async () => {
-    try {
-      // Use the 'answerquestion' endpoint to submit answers
-      const response = await fetch(buildPath('/api/answerquestion'), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ answers: userAnswers }),
-      });
-
-      const result = await response.json();
-
-      if (result.success) {
-        // Use the 'completetest' endpoint to complete the test
-        const completeTestResponse = await fetch(buildPath('/api/completetest'), {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({}),
-        });
-
-        const completeTestResult = await completeTestResponse.json();
-
-        if (completeTestResult.success) {
-          // Handle successful completion
-          alert('Test submitted successfully.');
-        } else {
-          alert('Error completing the test.');
-        }
-      } else {
-        alert('Error submitting answers.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
   return (
-    <div id="exam">
-      <h1>Exam</h1>
-      {questions.length > 0 && (
-        <>
-          <p>Total Questions: {questions.length}</p>
-          {questions.map((question) => (
-            <div key={question.questionId}>
-              <p>{question.questionText}</p>
-              <ul>
-                {question.answerOptions.map((option) => (
-                  <li
-                    key={option.answerId}
-                    onClick={() => handleAnswerClick(question.questionId, option.answerId)}
-                  >
-                    {option.answerText}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-          <button onClick={handleSubmitTest}>Submit Test</button>
-        </>
-      )}
+    <div>
+      <h1>Test Name</h1>
+      {questions.map((question) => (
+        <div key={question._id}>
+          <h3>{question.Question}</h3>
+          <ul>
+            {question.Answers.map((answer, index) => (
+              <li key={index} onClick={() => handleAnswerClick(question._id, answer)}>
+                {answer}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <button onClick={handleCompleteTest} disabled={isTestCompleted}>
+        {isTestCompleted ? 'Test Completed' : 'Complete Test'}
+      </button>
     </div>
   );
+
+
+
 };
 
 export default Exam;
