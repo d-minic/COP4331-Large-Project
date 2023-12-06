91531424d81949315e7bf9174eafbbddfc6720d0
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-05 21:56:02 -0500

    Update EditProfile.js

--- frontend/src/components/EditProfile.js
+++ frontend/src/components/EditProfile.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './EditProfile.css'; // Import the CSS file
 
 const EditProfile = () => {
   // Retrieve the stored user information from local storage
