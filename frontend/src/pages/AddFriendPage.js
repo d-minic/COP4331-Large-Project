d93474694631d5139f3b12c1cd3c8e7246b789d6
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-05 19:28:13 -0500

    Create AddFriendPage.js

---
+++ frontend/src/pages/AddFriendPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import AddFriend from '../components/AddFriend';
+const AddFriendPage = () =>
+{
+    return(
+        <div>
+        <AddFriend/>
+        </div>
+    );
+};
+export default AddFriendPage;
