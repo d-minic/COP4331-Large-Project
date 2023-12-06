1b4bc3eb3b5777438f99f42ada2837e214e11498
Alex <acharkiewicz27@gmail.com>
2023-12-05 21:53:58 -0500

    Update Browse.js

--- frontend/src/components/Browse.js
+++ frontend/src/components/Browse.js
@@ -3,6 +3,29 @@ import { Link } from 'react-router-dom';
 import './Browse.css'; 
 import logo from './smarttoothlesspixel.PNG'; 
 
+function test(titleString, authorString)
+{
+    const grid = document.getElementById("grid");
+
+    const cardTempA = document.createElement("a");
+    cardTempA.setAttribute('href', 'https://www.example.com');
+    cardTempA.setAttribute('class', 'cardLink');
+
+    const cardTemp = document.createElement("div");
+    cardTemp.classList.add("card");
+
+    const title = document.createElement("p");
+    title.innerText = titleString;
+    cardTemp.append(title);
+
+    const author = document.createElement("p");
+    author.innerText = authorString;
+    cardTemp.append(author);
+    
+    cardTempA.append(cardTemp); 
+
+    grid.appendChild(cardTempA);
+}
 function Browse() {
     return (
     <div id="browseDiv">
@@ -19,6 +42,17 @@ function Browse() {
                 
             </ul>
         </nav>
+
+        <button onclick = "test('Chemistry 1', 'Joe')">New Card</button>
+        
+        <div class = "grid" id="grid">
+            <a href="", class = "cardLink">
+                <div class="card">
+                    <p class = "name">Biology 1</p>
+                    <p class = "name">Steve</p>
+                </div>
+            </a>
+        </div>
     
     </div>
     );
