ca19db49f6ccef44b32fd0322ccd62db9875367a
AmberLett <125529812+AmberLett@users.noreply.github.com>
2023-12-05 19:30:50 -0500

    Update Friends.js

--- frontend/src/components/Friends.js
+++ frontend/src/components/Friends.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './Friends.css'; // Import the CSS file
 const userId = "656d0128aaa2ae92a2d981b7";
 
